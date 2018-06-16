46cd5b56d5f4de0f400ed5e8e1de06fd76c229c1
Sri <k3rn3l16@outlook.com>
2018-06-15 19:48:24 -0700

    Fix overdraw & format code

--- app/src/main/java/com/example/android/flixtrove/ui/MainActivity.java
+++ app/src/main/java/com/example/android/flixtrove/ui/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.flixtrove.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -12,55 +13,58 @@ import com.example.android.flixtrove.ui.detail.MovieDetailFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		// Get a reference to the fragment manager to add the list view fragment to screen
-		FragmentManager fragmentManager = getSupportFragmentManager();
+        // Get a reference to the fragment manager to add the list view fragment to screen
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment listFragment = fragmentManager.findFragmentById(R.id.fragment_container);
 
-		// Initialize list view fragment
-		MoviesListFragment listFragment = new MoviesListFragment();
+        // Initialize list view fragment
+        if (listFragment == null) {
+            listFragment = new MoviesListFragment();
 
-		// Fragment transaction
-		fragmentManager.beginTransaction()
-				.add(R.id.fragment_container, listFragment)
-				.commit();
-	}
+            // Fragment transaction
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, listFragment)
+                    .commit();
+        }
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
-	@Override
-	public boolean onPrepareOptionsMenu(Menu menu) {
-		Intent intent = getIntent();
-		// Disable sort menu for detailed pane
-		if (intent != null && intent.hasExtra(MovieDetailFragment.INTENT_MOVIE_ID)) {
-			menu.removeItem(R.id.action_sort);
-		}
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        Intent intent = getIntent();
+        // Disable sort menu for detailed pane
+        if (intent != null && intent.hasExtra(MovieDetailFragment.INTENT_MOVIE_ID)) {
+            menu.removeItem(R.id.action_sort);
+        }
 
-		// Return modified menu
-		return super.onPrepareOptionsMenu(menu);
-	}
+        // Return modified menu
+        return super.onPrepareOptionsMenu(menu);
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		int id = item.getItemId();
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
 
-		switch (id) {
-			case R.id.action_top_rated:
-				// Fragment handles this
-				return false;
-			case R.id.action_popular:
-				// Fragment handles this
-				return false;
-		}
+        switch (id) {
+            case R.id.action_top_rated:
+                // Fragment handles this
+                return false;
+            case R.id.action_popular:
+                // Fragment handles this
+                return false;
+        }
 
-		// Android framework handles default
-		return super.onOptionsItemSelected(item);
-	}
+        // Android framework handles default
+        return super.onOptionsItemSelected(item);
+    }
 }
